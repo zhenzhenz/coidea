20f952e65c4de10609a85c7468517196b24d35e1
majiaxin110 <majiaxin110@gmail.com>
2021-08-28 16:14:11 +0800

    适配0.6.0

--- src/main/java/sse/tongji/coidea/view/RepositoryEditorImpl.java
+++ src/main/java/sse/tongji/coidea/view/RepositoryEditorImpl.java
@@ -4,16 +4,27 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.LocalFileSystem;
+import dev.mtage.eyjaot.client.inter.util.GeneralFileIgnoreUtil;
+import dev.mtage.eyjaot.client.inter.util.MyLogger;
 import dev.mtage.eyjaot.client.inter.view.GeneralRepositoryEditorImpl;
+import org.apache.commons.io.FileUtils;
 import sse.tongji.coidea.listener.MyRepositoryListener;
+import sse.tongji.coidea.presenter.LocalRepositoryPresenter;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author mtage
  * @since 2021/3/18 14:52
  */
+
 public class RepositoryEditorImpl extends GeneralRepositoryEditorImpl {
+    private final MyLogger log = MyLogger.getLogger(LocalRepositoryPresenter.class);
     private final Project project;
 
     public RepositoryEditorImpl(Project project) {
@@ -21,8 +32,10 @@ public class RepositoryEditorImpl extends GeneralRepositoryEditorImpl {
     }
 
     @Override
-    public String getDefaultProjectPath() {
-        return project.getBasePath();
+    public Path getDefaultProjectPath() {
+        return Optional.ofNullable(project.getBasePath())
+                .map(Path::of)
+                .orElse(null);
     }
 
     @Override
@@ -32,6 +45,13 @@ public class RepositoryEditorImpl extends GeneralRepositoryEditorImpl {
 
     @Override
     public void syncDataToDefault(byte[] repoData) throws IOException {
+        for (Path eachFile : Files.list(getDefaultProjectPath()).collect(Collectors.toList())) {
+            if (GeneralFileIgnoreUtil.isPathIgnored(eachFile)) {
+                continue;
+            }
+            log.info("删除本地文件 {0}", eachFile.toString());
+            FileUtils.forceDelete(eachFile.toFile());
+        }
         super.syncDataToDefault(repoData);
     }
 
